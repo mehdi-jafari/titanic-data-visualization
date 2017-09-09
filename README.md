f66425b9d4669838809e8c10cdd740e64f65663d
mehdi-jafari <jafarinejad_m@hotmail.com>
2017-09-09 13:01:02 -0400

    Feat : Add first feadback section to the report

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# titanic-data-visualization
-titanic dataset visualization using d3.js and dimle.js
+# Titanic dataset visualization using d3.js and dimle.js
+
+## Summary 
+RMS Titanic was a British passenger liner that sank in the North Atlantic Ocean in the early morning hours of 15 April 1912, 68% of the passengers on board perished. However, there is a difference in the survival chance among passengers across passenger socioeconomic status, age, gender and the fact that if they are traveling alone or with their family.  The goal of this visualization is representing this difference in a way so that the viewers can easily explore the situations.
+
+## Design 
+Objective:
+The story is to visualize the chance of survival across different passenger categories like socioeconomic status, age, and gender. The data I want to encode in the charts include passenger class, gender, whether they are children,  whether they survived or perished in this catastrophe.
+
+### Data Cleaning:
+The original data was already cleaned but I had to generate some factor variables based on the original data like categorizing their age into four different groups (Children, Youth, Adults, and Seniors) and also whether they are traveling with their family or alone based on the number of siblings and children on the board.
+
+### Chart Selections:
+* Bar chart is chosen because its best usage is for comparison of different categories. 
+* Stacked bar chart uses color to encode the grouped data for each category, then it was used for encoding survived and died per category.
+
+
+### Visual Encoding:
+* x position: passenger category.
+* y position: the counts or the percentage of the passenger. 
+* color hue: survived or died.
+
+
+### Library Selecting
+the primary visual library used in this project is dimple.js for creating these charts. The aim of dimple is to open up the power and flexibility of d3 to analysts. It aims to give a gentle learning curve and minimal code to achieve something productive. It also exposes the d3 objects so you can pick them up and run to create some really cool stuff.
+
+## Feedback
+Feedbacks are collected through in person interview and the suggestion for improvements are considered in the final chart.
+
+* Feedback 1
+	* Documenting JavaScript with JSDoc
+	* Add summary section in the README.md
+	* Add design section to explain the reasoning behind the visualization decisions like why bar chart and why stacked bar chart?
+
+* Feedback 2
+
+* Feedback 3
+
+## Resources 
+* Titanic information: https://en.wikipedia.org/wiki/RMS_Titanic
+* Dimple js : http://dimplejs.org/
+* Dimple js examples: http://dimplejs.org/examples_index.html
+* Documenting javaScript with JSDoc: http://usejsdoc.org/about-commandline.html
--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
+     /** This function generates bar chart based on the paremters. 
+     * @param {json} data - The data that need to be processesd.
+     * @param {string} contanier_name - The containerName that charts will be generated in.
+     * @param {string} x_axis - The variable name that will be used in X axis.
+     * @param {string} x_title - Y axis title
+     * @param {string} y_axis - The variable name that will be used in Y axis.
+     * @param {string} y_title - Y axis title
+     * @param {string} title - The ttitle of the chart.
+     * @param {string[]} x_order_rules - The order of categories that should be used in ploting the bar chart .
+     * @param {string} groupby - This variable is used to groupby each category based on this variable. defualt is Null. 
+     * @param {boolean} show_legend - whether add legend to the chart
+     * @param {string} use_pct - wether the Y axis will be measured and a proportional numerical axis will be created     
+     */ 
     function generate_distribution_chart(data, contanier_name, x_axis, x_title, y_axis, y_title, title, x_order_rules, groupby, show_legend,use_pct ){
       var margin = 40
       scaler = 0.5
@@ -32,22 +45,28 @@
       .style("font-weight", "bold").text(title);
     }
 
+    /** This function generates bar chart for all sections. 
+     * @param {json} data - The data that need to be processesd.
+     */
     function draw_distributions(data) {
-      /*D3.js setup code*/
+     /*D3.js setup code*/      
 
-      "use strict";
+     //generate the destibution for selected variables
       generate_distribution_chart(data,"#distribution","Sex", "Gender", "PassengerId", "Number of Passengers", "Gender Distribution", ['male', "female"]);
       generate_distribution_chart(data,"#distribution","socioeconomic", "socio-economic Status", "PassengerId", "Number of Passengers", "Socio-economic Distribution", ['Lower', "Middle", "Upper"]);
       generate_distribution_chart(data,"#distribution","AgeCategory", "Age Category", "PassengerId", "Number of Passengers", "Age Distribution", ['Children', "Youth", "Adults", "Seniors", "UnKnown"]);
       generate_distribution_chart(data,"#distribution","WithFamily", "Traveling With Family Status", "PassengerId", "Number of Passengers", "Traveling With Family Status Distribution", ['With Family', 'Alone']);
 
+      //genaret grouped distribution by survival status for selected variables 
       generate_distribution_chart(data,"#grouped_distribution","Sex", "Gender", "PassengerId", "Number of Passengers", "Gender Distribution", ['male', "female"], "SurvivedFactor", true);
       generate_distribution_chart(data,"#grouped_distribution","socioeconomic", "socio-economic Status", "PassengerId", "Number of Passengers", "Socio-economic Distribution", ['Lower', "Middle", "Upper"], "SurvivedFactor", true);
       generate_distribution_chart(data,"#grouped_distribution","AgeCategory", "Age Category", "PassengerId", "Number of Passengers", "Age Distribution", ['Children', "Youth", "Adults", "Seniors", "UnKnown"], "SurvivedFactor", true);
       generate_distribution_chart(data,"#grouped_distribution","WithFamily", "Traveling With Family Status", "PassengerId", "Number of Passengers", "Traveling With Family Status Distribution", ['With Family', 'Alone'], "SurvivedFactor", true);
 
+      //genaret grouped distribution by survival status in percentage for selected variables 
       generate_distribution_chart(data,"#pct_distribution","Sex", "Gender", "PassengerId", "Number of Passengers", "Gender Distribution", ['male', "female"], "SurvivedFactor", true, true);
       generate_distribution_chart(data,"#pct_distribution","socioeconomic", "socio-economic Status", "PassengerId", "Number of Passengers", "Socio-economic Distribution", ['Lower', "Middle", "Upper"], "SurvivedFactor", true, true);
       generate_distribution_chart(data,"#pct_distribution","AgeCategory", "Age Category", "PassengerId", "Number of Passengers", "Age Distribution", ['Children', "Youth", "Adults", "Seniors", "UnKnown"], "SurvivedFactor", true, true);
       generate_distribution_chart(data,"#pct_distribution","WithFamily", "Traveling With Family Status", "PassengerId", "Number of Passengers", "Traveling With Family Status Distribution", ['With Family', 'Alone'], "SurvivedFactor", true, true);
+    
     };
